cf34bb40c9e79c4f23097dbb23db47d605530c27
Sean <sean23@gmail.com>
2024-01-12 10:47:23 -0500

    fixed parameter types in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,10 +2,11 @@
 
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: { params: { num: string; }; }, res: { send: (arg0: string) => void; }) => {
+  const {num} = req.params;
 
   const fibN = fibonacci(parseInt(num));
+
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
